9dd5cf59be417dcbdc375a8dec1c4ab139302300
Sammy Ferreira <sammyfe2021@gmail.com>
2023-09-18 12:53:00 -0300

    :recycle: refactor: complete tasks style

--- src/components/tasks/Task.tsx
+++ src/components/tasks/Task.tsx
@@ -20,6 +20,7 @@ const Task = ({ nome, hora, feito, id, desc, dias }: TaksProps) => {
   const cookies = parseCookies()
   const jwt = cookies.jwtToken
 
+  const [feitoP, setFeitoP] = useState(feito)
   const [openModal, setOpenModal] = useState(false)
 
   const deleteTask = async () => {
@@ -34,74 +35,54 @@ const Task = ({ nome, hora, feito, id, desc, dias }: TaksProps) => {
   }
 
   const completeTask = async () => {
-    feito = !feito
+    const newFeito = !feito
+    console.log('1')
+    feito = newFeito
+    console.log('2')
+    setFeitoP(newFeito)
+    console.log('3')
+
     await axios.put('http://localhost:6969/update', {
       nome,
       hora,
       jwt,
       id,
-      feito,
+      feito: newFeito,
       dias,
       desc,
     })
-    window.location.reload()
   }
 
   const [hours, minutes] = hora.split(':')
 
   return (
     <div className="inline-flex w-full items-start justify-start gap-3 rounded p-1 shadow">
-      {!feito && (
-        <div className="flex w-full flex-row items-center justify-start gap-3 rounded bg-neutral-900 p-5 shadow">
-          <div>
-            <Checkbox
-              className="h-4 w-4"
-              onClick={completeTask}
-              checked={feito}
-            />
-          </div>
-          <div className="shrink grow basis-0 text-sm font-normal leading-tight text-zinc-100">
-            {nome}
-          </div>
-          <div className="flex shrink grow basis-0 flex-row items-center gap-2 text-sm font-normal leading-tight text-zinc-100">
-            <Clock className="h-4 w-4" />
-            {hours} : {minutes}
-          </div>
-
-          {desc !== null && (
-            <div className="flex shrink grow basis-0 gap-2 text-sm font-normal leading-tight text-zinc-100">
-              {desc !== '' && <ScrollText className="h-4 w-4" />}
-              {desc}
-            </div>
-          )}
-          <div className="flex flex-row gap-4">
-            <button onClick={() => setOpenModal(!openModal)}>
-              <Edit />
-            </button>
-            <button onClick={deleteTask}>
-              <Trash />
-            </button>
-          </div>
+      <div className="flex w-full flex-row items-center justify-start gap-3 rounded bg-neutral-900 p-5 shadow">
+        <div onClick={completeTask}>
+          <Checkbox className="h-4 w-4" checked={feitoP} />
         </div>
-      )}
-      {feito && (
-        <div className="flex w-full flex-row items-center justify-start gap-3 rounded bg-neutral-900 p-5 shadow">
-          <div>
-            <Checkbox onClick={completeTask} checked={feito} />
-          </div>
+        {feitoP && (
           <div className="shrink grow basis-0 text-sm font-normal leading-tight text-zinc-100 line-through">
             {nome}
           </div>
-          <div className="flex shrink grow basis-0 flex-row items-center gap-2 text-sm font-normal leading-tight text-zinc-100">
-            <Clock className="h-4 w-4" />
-            {hours} : {minutes}
+        )}
+        {!feitoP && (
+          <div className="shrink grow basis-0 text-sm font-normal leading-tight text-zinc-100">
+            {nome}
           </div>
-          {desc !== null && (
-            <div className="flex shrink grow basis-0 gap-2 text-sm font-normal leading-tight text-zinc-100">
-              {desc !== '' && <ScrollText className="h-4 w-4" />}
-              {desc}
-            </div>
-          )}
+        )}
+        <div className="flex shrink grow basis-0 flex-row items-center gap-2 text-sm font-normal leading-tight text-zinc-100">
+          <Clock className="h-4 w-4" />
+          {hours} : {minutes}
+        </div>
+
+        {desc !== null && (
+          <div className="flex shrink grow basis-0 gap-2 text-sm font-normal leading-tight text-zinc-100">
+            {desc !== '' && <ScrollText className="h-4 w-4" />}
+            {desc}
+          </div>
+        )}
+        <div className="flex flex-row gap-4">
           <button onClick={() => setOpenModal(!openModal)}>
             <Edit />
           </button>
@@ -109,7 +90,8 @@ const Task = ({ nome, hora, feito, id, desc, dias }: TaksProps) => {
             <Trash />
           </button>
         </div>
-      )}
+      </div>
+
       {openModal && (
         <EditTasks
           id={id}
